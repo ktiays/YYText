673df54de47f47aa34e1d264cccf309b6ad5472d
ktiays <cuansky@vip.qq.com>
2021-08-19 14:11:02 +0800

    add Swift Package Manager support

---
+++ Tests/YYTextTests/YYTextTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import YYText
+
+    final class YYTextTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            let _ = YYLabel()
+        }
+    }
